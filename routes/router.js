927d5687a5e1836da31e6824a2fb2d3e65c240eb
Eugene Vasileuski <zheka_1408@mail.ru>
2017-01-15 02:40:05 +0300

    #2 simple get request

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost/videos');
+
+var Video = mongoose.Schema({
+    idv: String,
+    thumbnail: Number,
+    aud : String,
+    title : String,
+    description : String
+});
+var db;
+var video = mongoose.model('video',Video);
+video.find({}, function (err, data) {
+    db = data;
+});
+
 
 router.get('/',function(req,res){
     res.render('Starter');
+});
+
+router.get('/videos', function (req, res) {
+    res.send(db);
 });
 
 module.exports = router;
