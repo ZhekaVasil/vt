dc0e0c3f8610a5c061769ba91f20b71df5c2b48f
Eugene Vasileuski <zheka_1408@mail.ru>
2017-01-14 19:33:46 +0300

    #1 created basic angular and node logics

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 var express = require('express'); //express
-var path = require('path'); // здесь будем ссылаться на физические файлы
-var logger = require('morgan');
-var cookieParser = require('cookie-parser'); //для поддержки сеансов
-var bodyParser = require('body-parser'); //для синтаксического разбора json
-var bcrypt = require('bcrypt-nodejs');
-var passport = require('passport'); //Используем passportjs для аутентификации
-var LocalStrategy = require('passport-local').Strategy; //используем стратегию паспорта
-var session = require('express-session'); // для поддержки сеансов
-var mongoose = require('mongoose'); //для mongodb, базы данных
-
-
 
 // импортируем маршрутизаторы
 var router = require('./routes/router');
@@ -22,19 +11,13 @@ var app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-//сообщаем узлу глобальную конфигурацию parser, logger и passport
-app.use(cookieParser());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // сообщаем узлу о тех каталогах, из которых приложение может брать ресурсы
 app.use('/', router);
 app.use(express.static('src'));
 
 
 //запускаем сервер на узле
-var server = app.listen(3000,'127.0.0.1', function () {
+var server = app.listen(3000, '127.0.0.1', function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
